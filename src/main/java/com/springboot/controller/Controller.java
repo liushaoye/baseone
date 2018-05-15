58f6bfd432f190a8b6eefea29db29882b5ffc18b
liuyang <shenshiyishao@163.com>
2018-05-15 10:18:37 +0800

    Initial commit

---
+++ src/main/java/com/springboot/controller/Controller.java
@@ -0,0 +1,25 @@
+package com.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by liuya
+ * User: liuya
+ * Date: 2018/5/15
+ * Time:  9:48
+ * projectName:baseone
+ */
+
+@RestController
+public class Controller {
+
+
+    @RequestMapping("/hello")
+    public String hello() {
+
+        return "Hello World";
+    }
+
+
+}
