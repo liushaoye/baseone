c0a83fa6bd2b09ad0d7dac3c7dccb16911a9ce1a
liuyang <shenshiyishao@163.com>
2018-05-15 13:54:33 +0800

    修改版本造成的冲突问题

--- src/test/java/com/springboot/BaseOneApplicationTests.java
+++ src/test/java/com/springboot/BaseOneApplicationTests.java
@@ -3,11 +3,9 @@ package com.springboot;
 import com.springboot.controller.Controller;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockServletContext;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -16,7 +14,6 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes = MockServletContext.class)
 @WebAppConfiguration
 public class BaseOneApplicationTests {
